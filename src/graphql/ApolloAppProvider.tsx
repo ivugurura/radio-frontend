7d60d2b178bb307077ae74eebaa6325f27aa824c
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-16 15:47:17 +0200

    Add notifier

--- src/graphql/ApolloAppProvider.tsx
+++ src/graphql/ApolloAppProvider.tsx
@@ -1,6 +1,7 @@
 import { type PropsWithChildren, useMemo } from 'react';
 import { ApolloProvider } from '@apollo/client/react';
 import { createApolloClient, defaultRefreshAccessToken } from './client';
+import { notifier } from '@libs/constants';
 
 type ApolloAppProviderProps = PropsWithChildren<{
   // Override defaults as needed (e.g., provide your own AsyncStorage token getters on RN)
@@ -23,12 +24,14 @@ export function ApolloAppProvider({
       includeCookies,
       // Optional: wire up telemetry/toasts/logging here
       onGraphQLError: (err, opName) => {
+        notifier.error(err.message);
         if (process.env.NODE_ENV !== 'production') {
           // eslint-disable-next-line no-console
           console.warn('[GraphQL error]', opName, err);
         }
       },
       onNetworkError: (err, opName) => {
+        notifier.error(`Network error`);
         if (process.env.NODE_ENV !== 'production') {
           // eslint-disable-next-line no-console
           console.warn('[Network error]', opName, err);
